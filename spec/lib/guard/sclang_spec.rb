852510790097ac2a712c97730a62b7bdf4768721
Adam Spiers <dotfiles@adamspiers.org>
2018-03-10 17:47:31 +0000

    properly test all_after_pass

--- spec/lib/guard/sclang_spec.rb
+++ spec/lib/guard/sclang_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe Guard::Sclang do
     fake_output = StringIO.new(test_output + "\n" + fake_summary + fake_footer)
     fake_output.rewind
     expect(PTY).to receive(:spawn).with(
-      "timeout", timeout.to_s, "sclang", %r{.*/unit-test-cli\.scd$}, paths
+      "timeout", timeout.to_s, "sclang", %r{.*/unit-test-cli\.scd$}, *paths
     ) { |command, *args, &block|
       `exit #{exitcode || fails}`
       block.call(fake_output, "fake stdin", $?.pid)
@@ -35,7 +35,7 @@ RSpec.describe Guard::Sclang do
     expect_output(paths, test_output, passes, fails, **extra)
     expect(Guard::Compat::UI).to receive(:notify).with(
       "#{passes} passes, #{fails} failures",
-      { title: paths, image: expected_success ? :success : :failed }
+      { title: paths.join(" "), image: expected_success ? :success : :failed }
     )
   end
 
@@ -60,7 +60,7 @@ RSpec.describe Guard::Sclang do
     end
 
     def test_success
-      expect_run("bar", dedent(<<-EOF), 5, 0, true)
+      expect_run(["bar"], dedent(<<-EOF), 5, 0, true)
         PASS: test passed
         EOF
       got_success = subject.run_all
@@ -68,7 +68,7 @@ RSpec.describe Guard::Sclang do
     end
 
     def test_failure
-      expect_run("bar", dedent(<<-EOF), 5, 1, false)
+      expect_run(["bar"], dedent(<<-EOF), 5, 1, false)
         PASS: test passed
         FAIL: test failed
         EOF
@@ -92,17 +92,30 @@ RSpec.describe Guard::Sclang do
   end
 
   describe "#run_on_modifications" do
-    it "handles zero failures" do
-      expect_run("baz", dedent(<<-EOF), 4, 0, true)
+    def expect_success(paths)
+      expect_run(paths, dedent(<<-EOF), 4, 0, true)
         PASS: test passed
         EOF
       expect_colored_text("PASS: test passed\n", :green)
+    end
+
+    def expect_failure(paths)
+      expect_run(paths, dedent(<<-EOF), 3, 2, false)
+        PASS: test passed
+        FAIL: test failed
+        EOF
+      expect_colored_text("PASS: test passed\n", :green)
+      expect_colored_text("FAIL: test failed\n", :red)
+    end
+
+    it "handles zero failures" do
+      expect_success(["baz"])
       success = subject.run_on_modifications(%w(baz))
       expect(success).to be == true
     end
 
     it "handles zero failures but non-zero exit code" do
-      expect_run("baz", dedent(<<-EOF), 4, 0, exitcode: 1)
+      expect_run(["baz"], dedent(<<-EOF), 4, 0, exitcode: 1)
         PASS: test passed
         EOF
       expect_colored_text("PASS: test passed\n", :green)
@@ -111,13 +124,57 @@ RSpec.describe Guard::Sclang do
     end
 
     it "handles failures" do
-      expect_run("qux", dedent(<<-EOF), 3, 2, false)
-        PASS: test passed
-        FAIL: test failed
-        EOF
-      expect_colored_text("PASS: test passed\n", :green)
-      expect_colored_text("FAIL: test failed\n", :red)
-      success = subject.run_on_modifications(%w(qux))
+      paths = ["qux"]
+      expect_failure(paths)
+      success = subject.run_on_modifications(paths)
+      expect(success).to be == false
+    end
+
+    it "triggers all_after_pass and everything passes" do
+      subject.options[:all_after_pass] = true
+
+      paths = ["qux"]
+
+      # fail the 1st run
+      expect_failure(paths)
+      success = subject.run_on_modifications(paths)
+      expect(success).to be == false
+
+      # Pass the 2nd; this should trigger 3rd run (run_all).
+      # Set up expectations for 2nd run:
+      expect_success(paths)
+
+      # Set up expectations for 3rd run to pass:
+      all = %w(foo bar baz qux)
+      expect(Guard::Compat).to receive(:matching_files).and_return(all)
+      expect_success(all)
+
+      # Now trigger 2nd and 3rd:
+      success = subject.run_on_modifications(paths)
+      expect(success).to be == true
+    end
+
+    it "triggers all_after_pass and final run_all fails" do
+      subject.options[:all_after_pass] = true
+
+      paths = ["qux"]
+
+      # fail the 1st run
+      expect_failure(paths)
+      success = subject.run_on_modifications(paths)
+      expect(success).to be == false
+
+      # Pass the 2nd; this should trigger 3rd run (run_all).
+      # Set up expectations for 2nd run:
+      expect_success(paths)
+
+      # Set up expectations for 3rd run to pass:
+      all = %w(foo bar baz qux)
+      expect(Guard::Compat).to receive(:matching_files).and_return(all)
+      expect_failure(all)
+
+      # Now trigger 2nd and 3rd:
+      success = subject.run_on_modifications(paths)
       expect(success).to be == false
     end
   end
